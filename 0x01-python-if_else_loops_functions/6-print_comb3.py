f6f4a444db6eda910c1e1b5a688f7b9541b84ea1
11121997 <nawalkhaled27@yahoo.com>
2023-10-03 15:39:30 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for n in range(10):
     for k in range(n, 10):
         if n < k:
-            print("{:d}{:d}".format(n, k), end="\n" if n == 8 and k == 9 else ", ")
+            print("{:d}{:d}".format(n, k),
+                    end="\n" if n == 8 and k == 9 else ", ")
