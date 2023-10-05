3a7b5194e7c1d384f1f0c87a8ffa99d84f65915b
11121997 <nawalkhaled27@yahoo.com>
2023-10-05 13:52:58 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for k in range(len(sys.av) - 1):
-        sum += int(sys.av[k+1])
+    for k in range(len(sys.argv) - 1):
+        sum += int(sys.argv[k+1])
     print(sum)
