5300480077e3aa99400df28002ecd4cb4defbf3a
11121997 <nawalkhaled27@yahoo.com>
2023-10-08 10:25:55 -0700

    update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return None
     new_list = []
     for k in my_list:
-        if k % 2 == 0:
+        if (k % 2) == 0:
             new_list.append(True)
         else:
             new_list.append(False)
