5c9c83598077b7da5c62431b3f06fc09430ea282
11121997 <nawalkhaled27@yahoo.com>
2023-10-08 09:56:01 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
+    if len(sentence):
+        return None
     a1 = len(sentence)
     a2 = sentence[0]
     tup = (a1, a2)
