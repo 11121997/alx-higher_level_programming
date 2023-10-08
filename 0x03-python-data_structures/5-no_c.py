edb89a0fa1c91d20f361f13b10692196deb6243b
11121997 <nawalkhaled27@yahoo.com>
2023-10-08 06:55:48 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-    for k in range(len(my_string):
+    for k in range(len(my_string)):
         if my_string[k] != c or my_string[k] != C:
-        new_str += my_string[k]
+            new_str += my_string[k]
     return new_str
