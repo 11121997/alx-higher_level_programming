c6a43ca5cc970a3f7935c6485c63e7e6f36f8d27
11121997 <nawalkhaled27@yahoo.com>
2023-10-08 06:54:04 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for k in my_string:
-        if k == c or k == C:
-            continue
-        return my_string
+    new_str = ""
+    for k in range(len(my_string):
+        if my_string[k] != c or my_string[k] != C:
+        new_str += my_string[k]
+    return new_str
