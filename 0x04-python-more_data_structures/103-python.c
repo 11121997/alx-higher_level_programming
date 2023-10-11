ab72df35cc20c042268f3d615c2ec9be6736f803
11121997 <nawalkhaled27@yahoo.com>
2023-10-11 13:34:38 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,9 +4,8 @@ void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
 /**
- * print_python_list - function
- * that prints some basic info about Python lists
- * @p: pyobject list
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
  */
 void print_python_list(PyObject *p)
 {
@@ -25,20 +24,19 @@ void print_python_list(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		type = list->ob_item[i]->ob_type->tp_name;
-		printf("Element %d: %s\n ", i, type);
+		printf("Element %d: %s\n", i, type);
 		if (strcmp(type, "bytes") == 0)
 			print_python_bytes(list->ob_item[i]);
 	}
 }
 
 /**
- * print_python_bytes - functions that print
- * some basic info about Python lists and Python bytes objects
- * @p: pyobject list
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
  */
 void print_python_bytes(PyObject *p)
 {
-	unsigned char i, s;
+	unsigned char i, size;
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
@@ -48,18 +46,19 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	printf("  size: %d\n", ((PyVarObject *)p)->ob_size);
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
 	printf("  trying string: %s\n", bytes->ob_sval);
 
 	if (((PyVarObject *)p)->ob_size > 10)
-		s = 10;
+		size = 10;
 	else
-		s = ((PyVarObject *)p)->ob_size + 1;
-	printf("  first %d bytes: ", s);
-	for (i = 0; i < s; i++)
+		size = ((PyVarObject *)p)->ob_size + 1;
+
+	printf("  first %d bytes: ", size);
+	for (i = 0; i < size; i++)
 	{
 		printf("%02hhx", bytes->ob_sval[i]);
-		if (i == (s - 1))
+		if (i == (size - 1))
 			printf("\n");
 		else
 			printf(" ");
