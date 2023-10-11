156b8637859a175b915efaa28e5615fcf347aaa7
11121997 <nawalkhaled27@yahoo.com>
2023-10-11 13:24:14 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,8 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
 /**
  * print_python_list - function
  * that prints some basic info about Python lists
