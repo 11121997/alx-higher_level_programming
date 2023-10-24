4476edf2fd02c69f2f76aced75a1951e43a5d8fb
11121997 <nawalkhaled27@yahoo.com>
2023-10-24 13:16:55 -0700

    update

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """constructor.
+
+        arg:
+            size: length of a side.
+        raise:
+            TypeError: if size is not int
+            ValueError: if size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Area of the square
+        Reurn: the current square area
+        """
+        return self.__size ** 2
