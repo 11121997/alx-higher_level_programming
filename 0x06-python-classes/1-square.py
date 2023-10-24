073fee00b5d4a8357c0a0b3ed249f931842ea8b0
11121997 <nawalkhaled27@yahoo.com>
2023-10-24 13:03:29 -0700

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """constructor.
+
+        arg:
+            size: length of a side.
+        """
+        self.__size = size
+~      
