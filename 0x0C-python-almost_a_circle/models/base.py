09afc6a37622296a7d6768bdedbc454cc36e4b33
11121997 <nawalkhaled27@yahoo.com>
2023-11-13 11:49:05 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,7 @@ class Base:
     def from_json_string(json_string):
         """static method"""
         if json_string is None or not json_string:
-            return "[]"
+            return []
         else:
             return loads(json_string)
 
