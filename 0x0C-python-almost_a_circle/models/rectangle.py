ab9edd9ed30a98ca8dd68c007bb4e37d4006ae1b
11121997 <nawalkhaled27@yahoo.com>
2023-11-13 07:45:55 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """Class Rectangle inherits from Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(id)
@@ -20,7 +21,6 @@ class Rectangle(Base):
 
     @width.setter
     def widthy(self, value):
-        self.validation("width", value, False)
         self.__width = value
 
     @property
@@ -30,7 +30,6 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.validation("height", value, False)
         self.__height = value
 
     @property
@@ -39,7 +38,6 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        self.validation("x", value)
         self.__x = value
 
     @property
@@ -48,30 +46,4 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        self.validation("y", value)
         self.__y = value
-
-    def validation(self, name, value, k=True):
-        """validation of all setter methods"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if k and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
-        if not k and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-
-    def area(self):
-        """the area value of the Rectangle"""
-        return self.width * self.height
-
-    def display(self):
-        """prints in stdout the Rectangle instance"""
-        ch = '\n' * self.y + \
-            (' ' * self.x + '#' * self.width + '\n') * self.height
-        print(ch, end="")
-
-    def __str__(self):
-        '''Returns info about the rectangle in string'''
-        return '[{}] ({}) {}/{} - {}/{}'.format(type(self).__name__,
-                                                self.id, self.x, self.y,
-                                                self.width, self.height)
