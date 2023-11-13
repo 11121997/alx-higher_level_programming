84b5ec97623185d37953f92e456a2d1cb6e3b297
11121997 <nawalkhaled27@yahoo.com>
2023-11-13 12:40:32 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,17 @@ class Base:
             list_objs = [k.to_dictionary() for k in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as fi:
             fi.write(cls.to_json_string(list_objs))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            copy = Rectangle(1, 1)
+        elif cls is Square:
+            copy = Square(1)
+        else:
+            copy = None
+        copy.update(**dictionary)
+        return copy
