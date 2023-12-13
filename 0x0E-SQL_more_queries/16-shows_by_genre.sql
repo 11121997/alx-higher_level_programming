1aa32503005761c7da0ae69ee9f86b9f25510df8
11121997 <nawalkhaled27@yahoo.com>
2023-12-13 06:46:11 -0800

    update

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 -- from the database hbtn_0d_tvshows
 SELECT title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id=id
-LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genres_id
+LEFT JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 ORDER BY title, tv_genres.name;
