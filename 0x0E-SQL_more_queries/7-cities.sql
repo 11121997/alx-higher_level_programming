a08cade5b93c55c8aa769ee6a1644ff7877c9d4b
11121997 <nawalkhaled27@yahoo.com>
2023-12-13 03:31:33 -0800

    update

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	FOREIGN KEY(state_id) REFERENCES TO hbtn_0d_usa.states(id),
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL);
