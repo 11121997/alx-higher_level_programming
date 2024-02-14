a888f0c6511fa6f0236c889bcf9dd5a0d645d33b
11121997 <nawalkhaled27@yahoo.com>
2024-02-14 14:46:02 -0800

    a1

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('100-data').list;
+const list = require('./100-data').list;
 const array = list.map((e, i) => e * i);
 console.log(list);
 console.log(array);
