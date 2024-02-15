d635c95d825589d62dfe2d7fd4b2b641266e094c
11121997 <nawalkhaled27@yahoo.com>
2024-02-14 16:29:16 -0800

    3a

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// fs module to interact with file system
+const fs = require('fs');
+
+const fileA = fs.readFileSync(process.argv[2]).toString();
+const fileB = fs.readFileSync(process.argv[3]).toString();
+
+fs.writeFileSync(process.argv[4], fileA + fileB);
