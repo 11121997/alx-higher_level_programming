b3daf75bf6e3a5dfc54666df15c9746f29e6152d
11121997 <nawalkhaled27@yahoo.com>
2024-03-05 15:57:51 -0800

    up

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = argv[1]
+        load = {"q": q}
+
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=load)
+    try:
+        d = response.json()
+        if d == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(d.get("id"), d.get("name")))
+    ecxept ValueError:
+        print("Not a valid JSON")
