5bbe6c8758acc82ffbbc788037120aa48a7ffe99
11121997 <nawalkhaled27@yahoo.com>
2024-03-05 16:03:49 -0800

    up

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         q = ""
     else:
-        q = argv[1]
+        q = sys.argv[1]
     payload = {"q": q}
 
     url = "http://0.0.0.0:5000/search_user"
