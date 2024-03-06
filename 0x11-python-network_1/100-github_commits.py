fb41cd269149d68e894ae5c630c7566342cea2e3
11121997 <nawalkhaled27@yahoo.com>
2024-03-05 18:54:23 -0800

    up

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         print("{}: {}".format(
             commits[k].get("sha"),
             commits[k].get("commit").get("author").get("name")))
-        if k > 10:
+        if k > 9:
             break
