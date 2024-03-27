6ca8b8c495a43eb28f66ea4587ef20b1af084159
11121997 <nawalkhaled27@yahoo.com>
2024-03-26 17:49:32 -0700

    update

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// script that prints the number of movies
+// where the character “Wedge Antilles” is present
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (!err) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
